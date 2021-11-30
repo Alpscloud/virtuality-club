e0fe73e9d88c95fd41e7101a2c9c73505b566a01
alpscloud <winde100kg@gmail.com>
2021-11-30 21:25:58 +0200

    added several sections

--- src/js/common.js
+++ src/js/common.js
@@ -5,17 +5,7 @@ $(document).ready(function() {
 			timer; // for disable scroll
 	// ========= =========== =========== ===========
 
-	// Disable hover effect when client scrolles the page
-	$(window).on('scroll',function() {
-		clearTimeout(timer);
-		if(!body.hasClass('disable-hover')) {
-			body.addClass('disable-hover');
-		}
 
-		timer = setTimeout(function() {
-			body.removeClass('disable-hover');
-		}, 200);
-	});
 
 	// Toggle menu
 
@@ -42,6 +32,22 @@ $(document).ready(function() {
 	});
 
 
+	$('.js-open-game-details-btn').on('click', function(e) {
+		e.preventDefault();
+		$(this).parents('.js-game').find('.js-game-details').addClass('is-opened');
+	});
+
+	$('.js-close-game-details-btn').on('click', function(e) {
+		e.preventDefault();
+		$(this).parents('.js-game-details').removeClass('is-opened');
+	});
+
+
+	
+
+	//=============
+
+
 	if ($('#fullpage').length > 0) {
 		var anchorsArr = [];
 
@@ -56,9 +62,10 @@ $(document).ready(function() {
 			menu: '#pageNav',
 			lockAnchors: true,
 			anchors: anchorsArr,
-			responsiveHeight: true,
-			loopBottom: true,
-			loopTop: true
+			fitToSection: false,
+			scrollOverflow: true
+			
+			
 			
 		});
 
@@ -91,27 +98,172 @@ $(document).ready(function() {
 
 
 	if ($('.js-promo-slider').length > 0) {
-		var promoSlider = new Swiper('.js-promo-slider', {
-			loop: true,
-			speed: 1000,
-			navigation: {
-				nextEl: '.js-promo-slider-btn-next',
-			},
+
+		$('.js-promo-slider').slick({
+			infinite: true,
+			nextArrow: $('.js-promo-slider-btn-next'),
+			dots: false
 		});
+
 	}
 
 	if ($('.js-includes-slider').length > 0) {
-		var includesSlider = new Swiper('.js-includes-slider', {
-			loop: true,
-			speed: 400,
-			slidesPerView: 'auto',
-			spaceBetween: 30,
-			navigation: {
-				nextEl: '.js-includes-slider-btn-next',
-				prevEl: '.js-includes-slider-btn-prev',
-			},
+
+		$('.js-includes-slider').slick({
+			infinite: true,
+			slidesToShow: 4,
+			variableWidth: true,
+			nextArrow: $('.js-includes-slider-btn-next'),
+			prevArrow: $('.js-includes-slider-btn-prev'),
+			dots: false
+		});
+
+	}
+
+	if ($('.js-top-games-slider').length > 0) {
+
+		$('.js-top-games-slider').slick({
+			centerMode: true,
+			variableWidth: true,
+			nextArrow: $('.js-top-games-slider-btn-next'),
+			prevArrow: $('.js-top-games-slider-btn-prev'),
+		});
+
+	}
+
+
+
+
+	if ($('.js-catalog-games-slider').length > 0) {
+		
+		$('.js-catalog-games-slider').each(function() {
+
+			var self = $(this);
+
+			self.slick({
+				rows: 2,
+				infinite: true,
+				slidesToShow: 4,
+				nextArrow: self.parents('.catalog-games__slider').find('.js-catalog-games-slider-btn-next'),
+				prevArrow: self.parents('.catalog-games__slider').find('.js-catalog-games-slider-btn-prev'),
+			});
+
+		});
+
+		$('.js-catalog-games-categories-slider').slick({
+			infinite: true,
+			slidesToShow: 8,
+			nextArrow: $('.js-catalog-games-categories-slider-btn-next'),
+			prevArrow: $('.js-catalog-games-categories-slider-btn-prev'),
+		});
+
+
+		function initCatalogGamesSlider() {
+			$('.js-catalog-games-slider').slick({
+				rows: 2,
+				infinite: true,
+				slidesToShow: 4,
+				nextArrow: self.parents('.catalog-games__slider').find('.js-catalog-games-slider-btn-next'),
+				prevArrow: self.parents('.catalog-games__slider').find('.js-catalog-games-slider-btn-prev'),
+			});
+		}
+
+
+
+		$('.js-catalog-games-block').not(":first").hide();
+	
+
+		$('.js-catalog-category-btn').on('click', function(e) {
+			e.preventDefault();
+
+			$('.js-catalog-category-btn').removeClass('is-active');
+			$(this).addClass('is-active');
+
+			$('.js-catalog-games-block').hide();
+
+			$('.js-catalog-games-block').eq($(this).attr('data-index')).fadeIn().addClass('is-active');
+
+			$('.js-catalog-games-slider').slick('setPosition');
+
+		});
+
+	}
+
+
+	if ($('.js-gallery-slider').length > 0) {
+		$('.js-gallery-slider').slick({
+			infinite: true,
+			speed: 300,
+			fade: true,
+			cssEase: 'linear',
+			asNavFor: '.js-gallery-slider-thumbnails'
+		});
+
+		$('.js-gallery-slider-thumbnails').slick({
+			slidesToShow: 4,
+			slidesToScroll: 1,
+			asNavFor: '.js-gallery-slider',
+			dots: false,
+			nextArrow: $('.js-gallery-slider-thumbnails-btn-next'),
+			prevArrow: $('.js-gallery-slider-thumbnails-btn-prev'),
+			focusOnSelect: true
 		});
 	}
+
+	if ($('.js-stocks-slider').length > 0) {
+		$('.js-stocks-slider').slick({
+			rows: 4,
+			slidesToShow: 1,
+			nextArrow: $('.js-stocks-slider-btn-next'),
+			prevArrow: $('.js-stocks-slider-btn-prev'),
+		});
+	}
+
+	if ($('.js-sertificates-slider').length > 0) {
+		$('.js-sertificates-slider').slick({
+			infinite: true,
+			centerMode: true,
+			centerPadding: '0px',
+			slidesToShow: 2,
+			variableWidth: true,
+			nextArrow: $('.js-sertificates-slider-btn-next'),
+			prevArrow: $('.js-sertificates-slider-btn-prev'),
+		});
+	}
+
+	if ($('.js-subscription-slider').length > 0) {
+		$('.js-subscription-slider').slick({
+			infinite: true,
+			slidesToShow: 4,
+			nextArrow: $('.js-subscription-slider-btn-next'),
+			prevArrow: $('.js-subscription-slider-btn-prev'),
+		});
+	}
+
+	if ($('.js-faq-slider').length > 0) {
+		$('.js-faq-slider').slick({
+			infinite: true,
+			centerMode: false,
+			slidesToShow: 8,
+			nextArrow: $('.js-faq-slider-btn-next'),
+			prevArrow: $('.js-faq-slider-btn-prev'),
+		});
+	}
+
+	if ($('.js-feedbacks-slider').length > 0) {
+		$('.js-feedbacks-slider').slick({
+			infinite: true,
+			centerMode: false,
+			slidesToShow: 6,
+			nextArrow: $('.js-feedbacks-slider-btn-next'),
+			prevArrow: $('.js-feedbacks-slider-btn-prev'),
+		});
+	}
+	
+	
+
+
+	
 
 
 });
